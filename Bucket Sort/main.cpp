6084ed976899b83f6b2b261fa1973cc8474945b6
zhengzhenyu <zhengzhenyu@bytedance.com>
2016-09-07 20:42:06 +0800

    添加多线程版本
    Change-Id: I33af268073a55ba038aa8d8ffe1231c60323c1e6

--- Bucket Sort/main.cpp
+++ Bucket Sort/main.cpp
@@ -21,16 +21,17 @@ int main(int argc, const char * argv[]) {
     vector<double> a;
     vector<double> b;
     vector<int> c;
-    vector<int> d;
-    const int length = 100000;
+    const int length = 200000;
 	
     srand(static_cast<unsigned>(time(0)));
     for (int i = 0; i != length; ++i) {
         a.push_back((rand() % 10000) / 10000.0 - 0.5);
         b.push_back((rand() % 10000) / 100.0);
         c.push_back((rand() % 100000));
-        d.push_back((rand() % 100000));
     }
+    
+    vector<int> d(c);
+    vector<int> e(c);
     
     time_t st = clock();
 //    BucketSort(a);
@@ -38,23 +39,29 @@ int main(int argc, const char * argv[]) {
 //    cout << " cost:" << static_cast<double>(clock() - st) / CLOCKS_PER_SEC << endl;
 //    cout << SortCheck(a) << endl;
     
-    st = clock();
-    BucketSort(b, true, 100);
-//    PrintVector(b);
-    cout << "cost:" << static_cast<double>(clock() - st) / CLOCKS_PER_SEC << endl;
-    cout << SortCheck(b, true) << endl;
-    
-    st = clock();
-    BucketSort(c, true, 10000);
-//    PrintVector(c);
-    cout << "cost:" << static_cast<double>(clock() - st) / CLOCKS_PER_SEC << endl;
-    cout << SortCheck(c, true) << endl;
-    
+//    st = clock();
+//    BucketSort(b, true, 100);
+////    PrintVector(b);
+//    cout << "cost:" << static_cast<double>(clock() - st) / CLOCKS_PER_SEC << endl;
+//    cout << SortCheck(b, true) << endl;
+//    
+//    st = clock();
+//    BucketSort(c, true, 10000);
+////    PrintVector(c);
+//    cout << "cost:" << static_cast<double>(clock() - st) / CLOCKS_PER_SEC << endl;
+//    cout << SortCheck(c, true) << endl;
+//    
     st = clock();
     BucketSort(d, true, 1000);
     //    PrintVector(d);
     cout << "cost:" << static_cast<double>(clock() - st) / CLOCKS_PER_SEC << endl;
     cout << SortCheck(d, true) << endl;
+    
+    st = clock();
+    ThreadBucketSort(e, true, 1000);
+    //    PrintVector(d);
+    cout << "cost:" << static_cast<double>(clock() - st) / CLOCKS_PER_SEC << endl;
+    cout << SortCheck(e, true) << endl;
     
     return 0;
 }
